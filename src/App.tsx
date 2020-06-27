daaa1e9a802fa44de7ad0d64e7e677aef24d0431
Maxim <turbo3.6@cn.ru>
2020-06-27 23:09:35 +0700

    basic css grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className={"header"}>
+                <img src="https://svgsilh.com/svg/156859.svg" alt=""/>
+            </header>
+            <nav className={"navigation"}>
+                <ul>
+                    <li>Profile</li>
+                    <li>Message</li>
+                    <li>News</li>
+                    <li>Music</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
+            <div className={"content"}>
+                <div>
+                    <img src="https://demo.presscustomizr.com/wp-content/uploads/2016/12/ny_city-1348x500.jpg" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
