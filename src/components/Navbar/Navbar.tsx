9a142583ce8009ee5bde06c54c8e06b09fa9538a
Maxim <turbo3.6@cn.ru>
2020-07-04 17:45:12 +0700

    new white style css

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import s from "./Navbar.module.css";
 export function Navbar() {
     return (
         <nav className={s.navigation}>
-            <ul className={"list"}>
+            <ul className={s.list}>
                 <li className={s.item}>Profile</li>
                 <li className={s.item}>Message</li>
                 <li className={s.item}>News</li>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
     return (
         <div className={s.content}>
             <div>
-                <img src="https://demo.presscustomizr.com/wp-content/uploads/2016/12/ny_city-1348x500.jpg" alt=""/>
+                <img src="https://demo.presscustomizr.com/wp-content/uploads/2016/12/ny_city-1348x500.jpg" width="960" alt=""/>
             </div>
             <div>
                 ava + description
