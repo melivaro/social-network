95b6f593fe954a4a3f6be36b1d028441ddb4a4a5
Maxim <turbo3.6@cn.ru>
2020-07-05 20:55:02 +0700

    refactor MyPosts/Post components

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,17 +4,16 @@ import s from "./MyPosts.module.css";
 
 export function MyPosts() {
     return (
-        <div>
-            My posts
-            <div>
-                <textarea></textarea>
+        <div className={s.myPosts}>
+            <h3>My posts</h3>
+            <div className={s.createPublication}>
+                <textarea cols={30} rows={1} placeholder={"Что у вас нового?"}></textarea>
                 <button type="submit">Add post</button>
             </div>
-            <div className={"Posts"}>
+            <ul className={s.postsList}>
                 <Post message={"Hi, how are you?"} likesCounter={10}/>
                 <Post message={"It's my first post "} likesCounter={15}/>
-
-            </div>
+            </ul>
         </div>
     )
 }
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -8,13 +8,13 @@ type PostType = {
 
 export function Post(props: PostType) {
     return (
-        <div className={s.item}>
+        <li className={s.item}>
             <img src="https://image.freepik.com/free-vector/handdrawn-samurai-logo_78838-23.jpg" alt=""/>
             <p>{props.message}</p>
             <div>
                 <span>like</span>
                 <span>{props.likesCounter}</span>
             </div>
-        </div>
+        </li>
     )
 }
