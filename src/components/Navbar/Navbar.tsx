63de5d0d38755bb4adfcfd486d516b5e431bae92
Maxim <turbo3.6@cn.ru>
2020-07-05 12:11:36 +0700

    add Navbar.module selector active

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
         <nav className={s.navigation}>
             <ul className={s.list}>
                 <li className={s.item}>Profile</li>
-                <li className={s.item}>Message</li>
+                <li className={`${s.item} ${s.active}`}>Message</li>
                 <li className={s.item}>News</li>
                 <li className={s.item}>Music</li>
                 <li className={s.item}>Settings</li>
