8a2dee2cc285d6ea7d3b845148e189c573c8e589
Maxim <turbo3.6@cn.ru>
2020-07-05 17:17:29 +0700

    initial structure Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import s from "./Dialogs.module.css"
 
 export function Dialogs() {
     return (
-        <div>DIALOGS!!!</div>
+        <div className={s.dialogs}>
+            <ul className={s.dialogsList}>
+                <li className={`${s.dialogItem} ${s.active}`}>Maxim</li>
+                <li className={s.dialogItem}>Anna</li>
+                <li className={s.dialogItem}>Tayusha</li>
+            </ul>
+            <ul className={s.messagesList}>
+                <li className={s.messageItem}>Hi girls</li>
+                <li className={s.messageItem}>happy hoy</li>
+                <li className={s.messageItem}>YO!</li>
+            </ul>
+        </div>
     )
 }
