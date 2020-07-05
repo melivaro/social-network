56c7720640e1668d8fe13965162cd54de1044eab
Maxim <turbo3.6@cn.ru>
2020-07-05 14:11:57 +0700

    add 'yarn add react-router-dom' and 'yarn add @types/react-router-dom'

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <NavBar/>
-            <div className="app-wrapper-content">
-                <Profile/>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <NavBar/>
+                <div className="app-wrapper-content">
+                    <Profile/>
+                    <Dialogs/>
+                    <Route/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
